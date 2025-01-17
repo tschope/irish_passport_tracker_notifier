3bf0271361cfe84e2057dec18cfb25fb77f3f70a
Rodrigo Tschope <r_tschope@apple.com>
2025-01-17 00:49:44 +0000

    favicon bug

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,30 +7,26 @@ export default defineNuxtConfig({
           charset: 'utf-8',
           viewport: 'width=device-width, initial-scale=1',
           link: [
-              // Ícones para dispositivos Apple
-              { rel: 'apple-touch-icon', sizes: '57x57', href: '~/assets/images/apple-icon-57x57.png' },
-              { rel: 'apple-touch-icon', sizes: '60x60', href: '~/assets/images/apple-icon-60x60.png' },
-              { rel: 'apple-touch-icon', sizes: '72x72', href: '~/assets/images/apple-icon-72x72.png' },
-              { rel: 'apple-touch-icon', sizes: '76x76', href: '~/assets/images/apple-icon-76x76.png' },
-              { rel: 'apple-touch-icon', sizes: '114x114', href: '~/assets/images/apple-icon-114x114.png' },
-              { rel: 'apple-touch-icon', sizes: '120x120', href: '~/assets/images/apple-icon-120x120.png' },
-              { rel: 'apple-touch-icon', sizes: '144x144', href: '~/assets/images/apple-icon-144x144.png' },
-              { rel: 'apple-touch-icon', sizes: '152x152', href: '~/assets/images/apple-icon-152x152.png' },
-              { rel: 'apple-touch-icon', sizes: '180x180', href: '~/assets/images/apple-icon-180x180.png' },
+              { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' },
+              { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' },
+              { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' },
+              { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' },
+              { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-icon-114x114.png' },
+              { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-icon-120x120.png' },
+              { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png' },
+              { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png' },
+              { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png' },
 
-              // Ícones para Android
-              { rel: 'icon', type: 'image/png', sizes: '192x192', href: '~/assets/images/android-icon-192x192.png' },
-              { rel: 'icon', type: 'image/png', sizes: '32x32', href: '~/assets/images/favicon-32x32.png' },
-              { rel: 'icon', type: 'image/png', sizes: '96x96', href: '~/assets/images/favicon-96x96.png' },
-              { rel: 'icon', type: 'image/png', sizes: '16x16', href: '~/assets/images/favicon-16x16.png' },
+              { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-icon-192x192.png' },
+              { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+              { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+              { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
 
-              // Manifest
               { rel: 'manifest', href: '/manifest.json' },
           ],
           meta: [
-              // Meta tags
               { name: 'msapplication-TileColor', content: '#ffffff' },
-              { name: 'msapplication-TileImage', content: '~/assets/images/ms-icon-144x144.png' },
+              { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
               { name: 'theme-color', content: '#ffffff' }
           ],
       }
